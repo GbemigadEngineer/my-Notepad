649f09566ba0fe6d90b2c02721e35aaf2de63039
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-03-10 13:45:52 +0100

    Installed dependecies like express, dotenv, etc

---
+++ server.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.configure({ path: "./config.env" });
+
+const app = require("./app.js");
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
