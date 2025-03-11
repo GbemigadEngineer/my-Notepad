aa6555c87f402f47674fe471b2bc9a68fd84970a
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-03-11 18:02:12 +0100

    Required mongoose and connected my local database to my app

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const noteRouter = require("./routes/notesroutes")
+dotenv.config({ path: "./config.env" });
+const noteRouter = require("./routes/notesroutes");
+
 const app = express();
 
+// Middlewear
+app.use(express.json());
 
-
-
-
-
-app.use(express.json())
+// Connect to MongoDB
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ Database connection failed:", err));
 
 // resource routers
 
-app.use("/api/v1/notes", noteRouter)
+app.use("/api/v1/notes", noteRouter);
 
-module.exports = app
-
+module.exports = app;
